9f93d47d70c2e73fa15e30fc9cbfccf70ce8115c
Saud <saud.s@engees.in>
2024-09-21 14:01:18 +0530

    vercel change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,24 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000
+  // app.enableCors({
+  //   origin: 'https://react-frontend-gray-five.vercel.app',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  //   credentials: true,
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  // });  
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
   app.enableCors({
-    origin: 'https://react-frontend-gray-five.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });  
+    allowedHeaders: "*",
+    origin: "*"
+  });
   await app.listen(port);
 }
 bootstrap();
