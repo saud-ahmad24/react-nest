771cd1bdc7388ce7f9974026a2a8e332e9de5173
Saud <saud.s@engees.in>
2024-09-21 14:02:33 +0530

    vercel change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 3000
   // app.enableCors({
   //   origin: 'https://react-frontend-gray-five.vercel.app',
@@ -11,17 +11,6 @@ async function bootstrap() {
   //   allowedHeaders: ['Content-Type', 'Authorization'],
   // });  
 
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: "*",
-    origin: "*"
-  });
   await app.listen(port);
 }
 bootstrap();
