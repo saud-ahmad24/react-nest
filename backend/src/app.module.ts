324f0fa24ecea672b7f4800f77aaa9d9a22dcda8
Saud <saud.s@engees.in>
2024-09-21 11:51:21 +0530

    All push

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,7 +5,7 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-todo-app'),
+    MongooseModule.forRoot('mongodb+srv://saudahmad24:v6FhgG0UPO3o1mbs@cluster0.gezq8.mongodb.net/'),
     AuthModule,
     TodoModule,
   ],
