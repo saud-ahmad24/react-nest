69811655dcff6cf00d2c7ca749030eb5f7825073
Saud <saud.s@engees.in>
2024-09-21 13:03:57 +0530

    vercel change

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow credentials if needed
   })
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
